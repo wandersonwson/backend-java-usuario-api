3840618fb2a5ee55f881d974755075fd432404dd
wandersonwson <wandersonwson@hotmail.com>
2024-06-19 22:34:09 -0300

    Início

---
+++ src/main/java/dev/wson/usuario_api/UsuarioApiApplication.java
@@ -0,0 +1,12 @@
+package dev.wson.usuario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioApiApplication.class, args);
+	}
+}
---
+++ src/test/java/dev/wson/usuario_api/UsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wson.usuario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
