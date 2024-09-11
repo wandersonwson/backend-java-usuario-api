4d31e541e692f1de2d079ffb1fdcc63480a0954b
wandersonwson <wandersonwson@hotmail.com>
2024-09-10 23:18:23 -0300

    Ajustes no controlador de usuários

--- src/main/java/dev/wson/usuario_api/controladores/UsuarioController.java
+++ src/main/java/dev/wson/usuario_api/controladores/UsuarioController.java
@@ -6,34 +6,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import dev.wson.usuario_api.modelos.UsuarioDTO;
 import dev.wson.usuario_api.servicos.UsuarioService;
 
 @RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    @PostMapping("/usuario")
+    @PostMapping
     public UsuarioDTO salvar(@RequestBody UsuarioDTO usuarioDTO) {
         return usuarioService.salvar(usuarioDTO);
     }
-    @GetMapping("/usuario")
+    @GetMapping
     public List<UsuarioDTO> buscarUsuarios() {
         List<UsuarioDTO> usuarios = usuarioService.buscarTodos();
         return usuarios;
     }
-    @GetMapping("/usuario/{id}")
+    @GetMapping("/{id}")
     public UsuarioDTO buscarPorId(@PathVariable long id) {
         return usuarioService.buscarPorId(id);
     }
-    @GetMapping("/usuario/{cpf}")
+    @GetMapping("/{cpf}")
     public UsuarioDTO buscarPorCpf(@PathVariable String cpf) {
         return usuarioService.buscarPorCpf(cpf);
     }
-    @GetMapping("/usuario/busca")
+    @GetMapping("/busca")
     public List<UsuarioDTO> buscarPorNome(@RequestParam(name = "nome", required = true) String nome) {
         return usuarioService.buscarPorNome(nome);
     }
