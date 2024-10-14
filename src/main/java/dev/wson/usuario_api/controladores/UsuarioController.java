7af101edccd4f6595bced7c9ae8815eb585babce
wandersonwson <wandersonwson@hotmail.com>
2024-10-13 23:00:07 -0300

    Correção do controlador de usuários

--- src/main/java/dev/wson/usuario_api/controladores/UsuarioController.java
+++ src/main/java/dev/wson/usuario_api/controladores/UsuarioController.java
@@ -2,13 +2,7 @@ package dev.wson.usuario_api.controladores;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import dev.wson.usuario_api.modelos.UsuarioDTO;
 import dev.wson.usuario_api.servicos.UsuarioService;
 
